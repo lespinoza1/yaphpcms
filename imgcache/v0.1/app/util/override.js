6e6aa208ac0589c25c4c5a8fcea6a4cac5cc207f
msl-138 <msl-138@163.com>
2013-02-02 08:04:56 +0000

    自定义操作column flex: 1，填满grid

--- imgcache/v0.1/app/util/override.js
+++ imgcache/v0.1/app/util/override.js
@@ -181,6 +181,7 @@ Ext.define('Yap.grid.column.Action', {
     alternateClassName: 'Yap.grid.ActionColumn',
     header: lang('OPERATE'),
     sortable: false,
+    flex: 1,
     columnCls: 'appactioncolumn',
     constructor: function(config) {
         var me = this,
