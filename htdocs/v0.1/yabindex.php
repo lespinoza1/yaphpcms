b4fe3c0fc4e8ae19fca6acb369b4736abfbd02c1
msl-138 <msl-138@163.com>
2013-02-18 06:54:07 +0000

    前台入口文件

---
+++ htdocs/v0.1/yabindex.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Yaphpcms前台入口文件
+ *
+ * @file            yabindex.php
+ * @package         Yap\Home
+ * @version         0.1
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-02-17 15:15:24
+ * @lastmodify      $Date$ $Author$
+ */
+
+define('APP_NAME'       , 'yablog');//项目名称
+require(__DIR__ . '/bootstrap.php');
