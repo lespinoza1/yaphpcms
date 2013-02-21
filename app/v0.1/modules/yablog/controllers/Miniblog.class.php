7657a2e7ebbb9641ad44d5ec77bc65e72b85a1c1
msl-138 <msl-138@163.com>
2013-02-21 10:01:27 +0000

    yablog前台模板

---
+++ app/v0.1/modules/yablog/controllers/Miniblog.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 微博控制器类
+ *
+ * @file            Miniblog.class.php
+ * @package         Yap\Module\Home\Controller
+ * @version         0.1
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-02-21 13:30:42
+ * @lastmodify      $Date$ $Author$
+ */
+
+class MiniblogController extends BaseController {
+    /**
+     * @var bool $_init_model true实例对应模型。默认false
+     */
+    protected $_init_model      = false;
+
+    /**
+     * 首页
+     *
+     * @author          mrmsl <msl-138@163.com>
+     * @date            2013-02-21 13:30:55
+     *
+     * @return void 无返回值
+     */
+    public function indexAction() {
+    }
+
+    /**
+     * 详请
+     *
+     * @author          mrmsl <msl-138@163.com>
+     * @date            2013-02-21 13:47:40
+     *
+     * @return void 无返回值
+     */
+    public function detailAction() {
+    }
+}
