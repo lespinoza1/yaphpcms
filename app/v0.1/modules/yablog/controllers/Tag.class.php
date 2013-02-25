9c4a005cf7507668a51a64c5684d7cb2a0e908bd
msl-138 <msl-138@163.com>
2013-02-25 04:10:29 +0000

    关于、联系、标签 控制器

---
+++ app/v0.1/modules/yablog/controllers/Tag.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 标签控制器类
+ *
+ * @file            Tag.class.php
+ * @package         Yap\Module\Home\Controller
+ * @version         0.1
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-02-25 09:54:41
+ * @lastmodify      $Date$ $Author$
+ */
+
+class TagController extends BaseController {
+    /**
+     * @var bool $_init_model true实例对应模型。默认false
+     */
+    protected $_init_model      = false;
+
+    /**
+     * 首页
+     *
+     * @author          mrmsl <msl-138@163.com>
+     * @date            2013-02-21 13:44:11
+     *
+     * @return void 无返回值。如果未登陆跳转至登陆页
+     */
+    public function indexAction() {
+    }
+}
