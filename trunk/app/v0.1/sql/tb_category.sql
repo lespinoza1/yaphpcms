f71924a933a240ea884cb59129435139e2e70581
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-03-25 10:04:46 +0000

    博客分类数据

---
+++ trunk/app/v0.1/sql/tb_category.sql
@@ -0,0 +1,15 @@
+﻿/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+
+INSERT INTO `tb_category` VALUES (1,'php','php',3,1,0,2,'3,1','keyword','descriptionphp,nodejs,jquery,js,mysql,mongodb,yaf,backbond,undercorephp,nodejs,jquery,js,mysql,mongodb,yaf,backbond,undercore');
+INSERT INTO `tb_category` VALUES (2,'nodejs','nodejs',3,1,0,2,'3,2','keyword','descriptiondescriptiondescriptiondescriptiondescriptiondescription');
+INSERT INTO `tb_category` VALUES (3,'开发语言','blog',0,1,3,1,'3','php,nodejs,jquery,js,mysql,mongodb,yaf,backbond,undercore','php,nodejs,jquery,js,mysql,mongodb,yaf,backbond,undercore,seo');
+INSERT INTO `tb_category` VALUES (6,'数据库','database',0,1,6,1,'6','databasedatabase','databasedatabase');
+INSERT INTO `tb_category` VALUES (7,'mysql','mysql',6,1,7,2,'6,7','databasedatabase','databasedatabase');
+INSERT INTO `tb_category` VALUES (8,'mongodb','mongodb',6,1,8,2,'6,8','databasedatabase','databasedatabase');
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
