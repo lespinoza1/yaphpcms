11327fa3facec9c5f12198f1227540a8cf9f13e7
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-03-26 09:19:50 +0000

    博客前端，未完成

--- trunk/imgcache/v0.1/admin/app/util/override.js
+++ trunk/imgcache/v0.1/admin/app/util/override.js
@@ -13,6 +13,7 @@ Ext.Loader.setConfig({//路径
     paths: {
         Yap: System.sys_base_admin_imgcache + 'app',
         'Yap.ux.TabPanel': System.sys_base_admin_imgcache + 'app/ux/Yap.ux.TabPanel.js',
+        'Yap.ux.Ueditor': System.sys_base_admin_imgcache + 'app/ux/Yap.ux.Ueditor.js',
         'Yap#container': System.sys_base_admin_imgcache + 'app/container/',
         'Yap#controller': System.sys_base_admin_imgcache + 'app/controller/',
         'Yap#store': System.sys_base_admin_imgcache + 'app/store/',
