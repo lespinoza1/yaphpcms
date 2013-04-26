ca9e12ae0e04c09c76c91366e584c8f96a36db45
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-04-26 15:32:35 +0000

    微博详情

--- trunk/app/v0.1/modules/yablog/controllers/Miniblog.class.php
+++ trunk/app/v0.1/modules/yablog/controllers/Miniblog.class.php
@@ -14,9 +14,9 @@
 
 class MiniblogController extends BaseController {
     /**
-     * @var bool $_init_model true实例对应模型。默认false
+     * @var bool $_init_model true实例对应模型。默认true
      */
-    protected $_init_model      = false;
+    protected $_init_model = true;
 
     /**
      * 首页
@@ -27,6 +27,7 @@ class MiniblogController extends BaseController {
      * @return void 无返回值
      */
     public function indexAction() {
+        $this->_display();
     }
 
     /**
@@ -34,9 +35,46 @@ class MiniblogController extends BaseController {
      *
      * @author          mrmsl <msl-138@163.com>
      * @date            2013-02-21 13:47:40
+     * @lastmodify      2013-04-26 23:13:01
      *
      * @return void 无返回值
      */
     public function detailAction() {
+        $blog_id = Filter::int('id', 'get');
+        $date    = Filter::int('date', 'get');
+
+        if (!$blog_id || !$date) {//非法参数
+            Logger::record(L('INVALID_PARAM') . "date=({$date}),id=($blog_id)", CONTROLLER_NAME);
+            $this->_showMessage('error' . $blog_id . $date, null, 404);
+        }
+
+        if ($blog_info = $this->_model->find($blog_id)) {
+
+            if (date('Ymd', $blog_info['add_time']) != $date) {//日期与id不匹配
+                Logger::record(L('INVALID_PARAM') . "date=({$date}),id=($blog_id)", CONTROLLER_NAME);
+                $this->_showMessage('error' . $blog_id . $date, null, 404);
+            }
+
+            $filename = str_replace(BASE_SITE_URL, WWWROOT, $blog_info['link_url']);
+            new_mkdir(dirname($filename));
+
+            $o = $this->_getViewTemplate('build_html')
+            ->assign('blog_info', $blog_info)//微博内容
+            ->assign(array(
+                'web_title'         => L('MINIBLOG,DETAIL') . ' | ' . L('MINIBLOG'),
+                //'seo_keywords'      => $blog_info['seo_keyword'],
+                //'seo_description'   => $blog_info['seo_description'],
+                //'tags'              => $this->tags($blog_info['seo_keyword']),
+                //'relative_blog'     => $this->_getRelativeBlog($blog_id, $blog_info['seo_keyword']),
+            ));
+
+            $content = $o->fetch(CONTROLLER_NAME, 'detail');
+            file_put_contents($filename, $content);
+            echo $content;
+
+        }
+        else {//微博不存在
+            $this->_showMessage(L('MINIBLOG,NOT_EXIST'), null, 404);
+        }
     }
 }
