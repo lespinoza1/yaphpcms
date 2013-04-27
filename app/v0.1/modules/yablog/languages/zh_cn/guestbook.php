6c1a2d439bb55f429d2de8d417092ebb542469ea
msl-138 <msl-138@163.com>
2013-04-27 03:21:22 +0000

    语言项

---
+++ app/v0.1/modules/yablog/languages/zh_cn/guestbook.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 留言模块语言中文包
+ *
+ * @file            guestbook.php
+ * @package         Yap\Module\Yablog\Language
+ * @version         0.1
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-04-27 09:24:02
+ * @lastmodify      $Date$ $Author$
+ */
+
+return array(
+    'MODULE_NAME_COMMENT' => L('GUESTBOOK')
+);
