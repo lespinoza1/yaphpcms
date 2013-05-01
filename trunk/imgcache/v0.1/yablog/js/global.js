f796d7438b7aa70ab30e1227076bbf9faf3f1c03
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-05-01 14:14:19 +0000

    非微博详情页，鼠标滑过微博，显示微博详情入口，同时隐藏添加时间

--- trunk/imgcache/v0.1/yablog/js/global.js
+++ trunk/imgcache/v0.1/yablog/js/global.js
@@ -26,3 +26,26 @@ function navDropdown() {
         dropdowns.hide();
     });
 }
+
+/**
+ * 非微博详情页，鼠标滑过微博，显示微博详情入口，同时隐藏添加时间
+ *
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-05-01 22:00:47
+ *
+ * @return void 无返回值
+ */
+function showMiniblogDetailLink() {
+
+    if ('undefined' == typeof(IS_MINIBLOG_DETAIL)) {
+        $('.miniblog-info').hover(function() {
+            var me = $(this);
+            me.find('.add_time').toggle();
+            me.find('.link').toggle();
+        }, function() {
+            var me = $(this);
+            me.find('.add_time').toggle();
+            me.find('.link').toggle();
+        });
+    }
+}
