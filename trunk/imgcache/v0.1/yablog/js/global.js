ca6dc8e6abc66376719cd57328309fccf8a0b32b
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-05-07 00:19:53 +0000

    seajs data-main直接global.js,少加载init.js

--- trunk/imgcache/v0.1/yablog/js/global.js
+++ trunk/imgcache/v0.1/yablog/js/global.js
@@ -8,6 +8,30 @@
  * @lastmodify      $Date$ $Author$
  */
 
+seajs.config({
+    plugins: ['shim'],
+    map: [
+        ['.js', '.js?' + new Date().getTime()]
+    ],
+    alias: {
+        lang: {//语言包
+            src: System.sys_base_site_url + 'static/js/lang/zh_cn.js'
+        },
+        jquery: {//jquery
+            src: System.sys_base_common_imgcache + 'js/jquery/jquery-1.9.1.min.js'
+        },
+        global: {//全局
+            src: System.sys_base_js_url + 'global.js',
+            deps: ['jquery']
+        },
+        common: {//通用函数库
+            src: System.sys_base_common_imgcache + 'js/common.js'
+        }
+    }
+});
+
+seajs.use(['jquery', 'global'], bootstrap);
+
 /**
  * 添加留言或者评论
  *
@@ -29,6 +53,25 @@ function addComments() {
         });
         return false;
     });
+}
+
+/**
+ * 启动函数
+ *
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2013-05-01 17:22:16
+ *
+ * @return void 无返回值
+ */
+function bootstrap() {
+    window.$html = $('html');
+    navDropdown();//下拉菜单
+    showMiniblogDetailLink();//非微博详情页，鼠标滑过微博，显示微博详情入口，同时隐藏添加时间
+    showCommentsReply();//鼠标滑过留言评论，显示回复
+    getMetaInfo();//获取博客,微博元数据,包括点击量,评论数等
+    addComments();//添加留言或者评论
+
+    $('#nav-' + NAV_ID).addClass('active');//高亮导航
 }
 
 /**
