47ab9545d21f11c127f275f89305f1de99cb1b3f
msl-138 <msl-138@163.com>
2013-05-09 10:01:41 +0000

    遗漏的无版本控制文件

---
+++ app/v0.1/modules/admin/models/Common.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 底层通用模型
+ *
+ * @file            Common.class.php
+ * @package         Yap\Module\Admin\Model
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2012-12-26 12:43:14
+ * @lastmodify      $Date$ $Author$
+ */
+
+//namespace Yap\Module\Admin\Model;
+
+/**
+ * 底层模型
+ *
+ * @package         Yap\Module\Admin\Model
+ * @copyright       Copyright (c) 2013 {@link http://www.yaphpcms.com yaphpcms} All rights reserved
+ * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
+ * @author          mrmsl <msl-138@163.com>
+ * @date            2012-12-26 12:43:14
+ * @lastmodify      $Date$ $Author$
+ */
+
+class CommonModel extends BaseModel {
+}
