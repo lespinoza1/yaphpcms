14cfdbf15b0aad32d05d470fe23f1021d082af0a
msl-138 <msl-138@163.com>
2013-05-20 05:34:44 +0000

    留言留言,评论 验证码控制 后台表单域

--- app/v0.1/caches/modules/Module.cache.php
+++ app/v0.1/caches/modules/Module.cache.php
@@ -1,19 +1,37 @@
 <?php
 //后台自动生成，请毋修改
-//最后更新时间:2013-05-20 08:27:37
+//最后更新时间:2013-05-20 13:29:14
 
 return array (
-  'module_admin_verifycode_case' => '1',
   'module_admin_verifycode_enable' => '1',
-  'module_admin_verifycode_error_limit' => '',
+  'module_admin_verifycode_width' => '40',
   'module_admin_verifycode_height' => '20',
   'module_admin_verifycode_length' => '5',
+  'module_admin_verifycode_type' => '5',
+  'module_admin_verifycode_case' => '1',
   'module_admin_verifycode_order' => '4312',
   'module_admin_verifycode_refresh_limit' => '',
-  'module_admin_verifycode_type' => '5',
-  'module_admin_verifycode_width' => '40',
+  'module_admin_verifycode_error_limit' => '',
   'module_guestbook_check' => '1',
-  'module_guestbook_max_reply_level' => '5',
   'module_comments_check' => '1',
+  'module_guestbook_max_reply_level' => '5',
   'module_comments_max_reply_level' => '5',
+  'module_guestbook_verifycode_error_limit' => '',
+  'module_guestbook_verifycode_refresh_limit' => '',
+  'module_guestbook_verifycode_order' => '4312',
+  'module_guestbook_verifycode_case' => '1',
+  'module_guestbook_verifycode_type' => '5',
+  'module_guestbook_verifycode_length' => '5',
+  'module_guestbook_verifycode_height' => '20',
+  'module_guestbook_verifycode_width' => '40',
+  'module_guestbook_verifycode_enable' => '1',
+  'module_comments_verifycode_error_limit' => '',
+  'module_comments_verifycode_refresh_limit' => '',
+  'module_comments_verifycode_order' => '4312',
+  'module_comments_verifycode_case' => '1',
+  'module_comments_verifycode_type' => '5',
+  'module_comments_verifycode_length' => '5',
+  'module_comments_verifycode_height' => '20',
+  'module_comments_verifycode_width' => '40',
+  'module_comments_verifycode_enable' => '1',
 );
