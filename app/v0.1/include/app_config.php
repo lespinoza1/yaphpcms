01715294d4b1c7a5eadd6131f67ad74a7aa88622
msl-138 <msl-138@163.com>
2013-05-29 08:46:24 +0000

    增加留言评论状态 常量

--- app/v0.1/include/app_config.php
+++ app/v0.1/include/app_config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 项目常量定义模板，修改此文件后，后台修改网站基本信息，重新生成后生效。生成对应文件为app_config.php
- * 后台自动生成，请毋修改。最后更新时间: 2013-05-27 08:17:03
+ * 后台自动生成，请毋修改。最后更新时间: 2013-05-29 11:12:32
  *
  * @file            app_config.tpl
  * @package         Yap
@@ -10,7 +10,7 @@
  * @license         http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
  * @author          mrmsl <msl-138@163.com>
  * @date            2013-05-04 08:41:37
- * @lastmodify      2013-05-27 08:17:03
+ * @lastmodify      2013-05-29 11:12:32
  */
 
 //核心配置
@@ -49,10 +49,13 @@ define('JSONP_CALLBACK'        , 'jsonpcallback');//jsonp 请求名称
 define('DEFAULT_LANG'          , 'zh_cn');//默认语言
 define('DEFAULT_TIMEZONE'      , 'asia/shanghai');//默认语言
 
-//评论留言类型 by mrmsl on 2013-05-13 13:30:23
-define('COMMENT_TYPE_GUESTBOOK'      , 0);//留言
-define('COMMENT_TYPE_BLOG'           , 1);//博客评论
-define('COMMENT_TYPE_MINIBLOG'       , 2);//微博评论
+//评论留言类型及审核状态 by mrmsl on 2013-05-13 13:30:23
+define('COMMENT_TYPE_GUESTBOOK'     , 0);//留言
+define('COMMENT_TYPE_BLOG'          , 1);//博客评论
+define('COMMENT_TYPE_MINIBLOG'      , 2);//微博评论
+define('COMMENT_STATUS_UNAUDITING'  , 0);//未审核
+define('COMMENT_STATUS_PASS'        , 1);//通过
+define('COMMENT_STATUS_UNPASS'      , 2);//不通过
 
 //自定义错误类型
 define('E_APP_EXCEPTION'      , 'E_APP_EXCEPTION');//异常
