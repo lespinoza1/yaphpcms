f71ad4913b3acfd09d6088799110535ea7d11c79
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-05-29 08:38:45 +0000

    增加留言评论状态 常量

--- trunk/app/v0.1/include/app_config.tpl.php
+++ trunk/app/v0.1/include/app_config.tpl.php
@@ -49,10 +49,13 @@ define('JSONP_CALLBACK'        , 'jsonpcallback');//jsonp 请求名称
 define('DEFAULT_LANG'          , 'zh_cn');//默认语言
 define('DEFAULT_TIMEZONE'      , 'asia/shanghai');//默认语言
 
-//评论留言类型 by mrmsl on 2013-05-13 13:30:23
-define('COMMENT_TYPE_GUESTBOOK'      , 0);//留言
-define('COMMENT_TYPE_BLOG'           , 1);//博客评论
-define('COMMENT_TYPE_MINIBLOG'       , 2);//微博评论
+//评论留言类型及审核状态 by mrmsl on 2013-05-13 13:30:23
+define('COMMENT_TYPE_GUESTBOOK'     , 0);//留言
+define('COMMENT_TYPE_BLOG'          , 1);//博客评论
+define('COMMENT_TYPE_MINIBLOG'      , 2);//微博评论
+define('COMMENT_STATUS_UNAUDITING'  , 0);//未审核
+define('COMMENT_STATUS_PASS'        , 1);//通过
+define('COMMENT_STATUS_UNPASS'      , 2);//不通过
 
 //自定义错误类型
 define('E_APP_EXCEPTION'      , 'E_APP_EXCEPTION');//异常
