405691f2a2b425e04016fde2f64cf24f8b9ce34e
msl-138 <msl-138@163.com>
2013-06-07 05:34:14 +0000

    搞不清之前为什么注释掉  //is_file($filename = $cache_dir . $action . $cache_id . C('HTML_SUFFIX')) && unlink($filename);//缓存文件

--- yaphpcms/v0.1/Core/Template.class.php
+++ yaphpcms/v0.1/Core/Template.class.php
@@ -179,7 +179,7 @@ class Template {
                 unlink($filename);
             }
 
-            //is_file($filename = $cache_dir . $action . $cache_id . C('HTML_SUFFIX')) && unlink($filename);//缓存文件
+            is_file($filename = $cache_dir . $action . $cache_id . C('HTML_SUFFIX')) && unlink($filename);//缓存文件
 
             if ($cache_id) {//同时清除多个
                 $cache_id = is_array($cache_id) ? $cache_id : explode(',', $cache_id);
