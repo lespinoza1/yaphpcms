bd9718805b7e1f270632745065f1751cc0b1d46d
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-06-07 09:59:06 +0000

    完善发送邮件类,公司虚拟机,无网络,未测试,还要继续

--- trunk/app/v0.1/lib/Mailer.class.php
+++ trunk/app/v0.1/lib/Mailer.class.php
@@ -18,9 +18,17 @@ require(PHPMAILER_PATH . 'class.phpmailer.php');
 
 class Mailer extends PHPMailer {
     /**
+     * @var object $_model 实例
+     */
+    private $_model = null;
+    /**
      * @var object $_db 数据库实例
      */
     private $_db = null;
+    /**
+     * @var object $_view_template 数据库实例
+     */
+    private $_view_template = null;
     /**
      * @var string $CharSet 邮件内容编码，默认utf-8
      */
@@ -47,22 +55,152 @@ class Mailer extends PHPMailer {
     public $SMTPAuth = true;
 
     /**
+     * 留言评论有回复，发邮件通知
+     *
+     * @author      mrmsl <msl-138@163.com>
+     * @date        2013-06-07 17:36:25
+     *
+     * @param array $info 邮件信息,array('email' => $email, 'subject' => $subject, 'content' => $content)
+     *
+     * @return true|string true发送成功，否则错误信息
+     */
+    private function _comments_at_email($info) {
+        $info['content'] = $this->_view_template->fetch('Mail', 'comments_at_email');
+        var_dump($info);exit;
+    }
+
+    /**
+     * 执行发送邮件
+     *
+     * @author      mrmsl <msl-138@163.com>
+     * @date        2013-06-07 17:05:04
+     *
+     * @param array $info 邮件信息,array('email' => $email, 'subject' => $subject, 'content' => $content)
+     *
+     * @return true|string true发送成功，否则错误信息
+     */
+    private function _doMail($info) {
+        $this->subject = $info['subject'];
+        $this->MsgHTML($info['content']);
+        $this->AddAddress($info['email']);
+
+        if ($this->Send()) {
+            return true;
+        }
+        else {
+            $this->_model->addLog(L('SEND,CN_YOUJIAN') . $info['email'] . "({$info['subject']})" . L('FAILURE'), LOG_TYPE_EMAIL);
+            return $this->ErrorInfo;
+        }
+    }
+
+    /**
+     * 重新发送邮件
+     *
+     * @author      mrmsl <msl-138@163.com>
+     * @date        2013-06-07 16:50:57
+     *
+     * @param int $history_id 邮件历史id
+     *
+     * @return true|string true发送成功，否则错误信息
+     */
+    private function _reMail($history_id) {
+        $info = $this->_model
+        ->field('email,subject,content')
+        ->table(TB_MAIL_HISTORY)
+        ->where('history_id=' . $history_id)
+        ->find();
+
+        if ($info) {
+            return $this->_doMail($info);
+        }
+        else {
+            $error  = L('MAIL_TEMPLATE,INFO') . "({$history_id})" . L('NOT_EXIST');
+            $log    = __METHOD__ . ': ' . __LINE__ . ',' . $error;
+            C('TRIGGER_ERROR', array($log, E_USER_ERROR, 'mail.error'));
+            $this->_model->addLog($log, LOG_TYPE_INVALID_PARAM);
+
+            return $error;
+        }
+    }
+
+    /**
      * 构造函数
      *
      * @author      mrmsl <msl-138@163.com>
      * @date        2013-06-05 17:42:41
      *
-     * @param object $db         数据库实例
-     * @param bool   $exceptions true可捕获发送异常。默认true
+     * @param object $_model            数据库实例
+     * @param object $view_template
+     * @param bool   $exceptions true可捕获发送异常。默认false
      *
      * @return void 无返回值
      */
-    public function __construct($db, $exceptions = true) {
+    public function __construct($_model, $view_template,$exceptions = false) {
         parent::__construct($exceptions);
         $this->SetLanguage('zh_cn', PHPMAILER_PATH . 'language/');
         $this->setConfig();
-        $this->_db = $db;
+        $this->_model = $_model;
+        $this->_view_template = $view_template;
     }
+
+    /**
+     * 发送邮件
+     *
+     * @author      mrmsl <msl-138@163.com>
+     * @date        2013-06-07 14:17:32
+     *
+     * @param mixed $mail_info 邮件模板名称或邮件历史id
+     * @param string $email 要发送的邮箱
+     *
+     * @return bool true ntud
+     */
+    public function mail($mail_info = null, $email = null) {
+
+        if (is_numeric($mail_info)) {
+           return $this->_reMail($mail_info);
+        }
+        elseif (is_array($mail_info)) {
+            return $this->_doMail($mail_info);
+        }
+        else {
+            static $mail_template_info = array();
+
+            if (!isset($mail_template_info[$mail_info])) {
+                $info = $this->_model
+                ->table(TB_MAIL_TEMPLATE)
+                ->where(array('template_name' => $mail_info))
+                ->field('subject,template_name')
+                ->find();
+
+                if (!$info) {
+                    $error  = L('MAIL_TEMPLATE,INFO') . "({$mail_info})" . L('NOT_EXIST');
+                    $log    = __METHOD__ . ': ' . __LINE__ . ',' . $error;
+                    C('TRIGGER_ERROR', array($log, E_USER_ERROR, 'mail.error'));
+                    $this->_model->addLog($log, LOG_TYPE_INVALID_PARAM);
+                    $mail_template_info[$mail_info] = false;
+
+                    return $error;
+                }
+
+                $info['email'] = $email;
+                $mail_template_info[$mail_info] = $info;
+            }
+
+            if ($info = $mail_template_info[$mail_info]) {
+                if (method_exists($this, $method = '_' . $mail_info)) {//_+邮件模板名即为发送方法
+                    return $this->$method($info);
+                }
+                else {
+                    $error  = L('MAIL_TEMPLATE,METHOD') . $method . L('NOT_EXIST');
+                    $log    = __METHOD__ . ': ' . __LINE__ . ',' . $error;
+                    C('TRIGGER_ERROR', array($log, E_USER_ERROR, 'mail.error'));
+                    $this->_model->addLog($log, LOG_TYPE_INVALID_PARAM);
+
+                    return $error;
+                }
+            }
+        }//end if
+    }//end mail
 
     /**
      * 设置邮箱配置
