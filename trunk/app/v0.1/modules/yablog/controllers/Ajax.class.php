c6bddf0e902970b8f60d9a1901f8e7386902b241
msl-138@163.com <msl-138@163.com@b670bb9a-5a17-f5c1-8e6f-00ba30008882>
2013-06-15 05:29:42 +0000

    _getPairsData 根据两字段组合值获取数据,如id及add_time匹配才能获取到数据,而不仅仅根据id

--- trunk/app/v0.1/modules/yablog/controllers/Ajax.class.php
+++ trunk/app/v0.1/modules/yablog/controllers/Ajax.class.php
@@ -19,70 +19,6 @@ class AjaxController extends CommonController {
     protected $_init_model      = true;
 
     /**
-     * 获取博客,微博元数据,包括点击量,评论数等
-     *
-     * @author          mrmsl <msl-138@163.com>
-     * @date            2013-05-02 16:19:49
-     *
-     * @param string $type  类型。blog:博客；1:微博
-     * @param array  $data  id及时间数组
-     *
-     * @return array 元数据
-     */
-    private function _getMetaInfo($type = 'blog', $data) {
-
-        if (!$data) {
-            return array();
-        }
-
-
-        $table_arr = array(
-            'blog'      => TB_BLOG,
-            'miniblog'  => TB_MINIBLOG,
-        );
-        $blog_arr = array();
-
-        foreach($data as $k => $v) {
-            $v_arr = explode('|', $v);
-
-            if (isset($v_arr[0], $v_arr[1]) && ($id = intval($v_arr[0])) && ($add_time = intval($v_arr[1]))) {
-                $blog_arr[$id] = $add_time;
-            }
-        }
-
-        if (!$blog_arr) {
-            $this->triggerError($log = __METHOD__ . ': ' . __LINE__ . ',' . L('INVALID_PARAM') . var_export($data, true));
-            $this->_model->addLog($log, LOG_TYPE_INVALID_PARAM);
-
-            return array();
-        }
-
-        $add_time_arr   = array_values($blog_arr);
-        $blog_id_arr    = array_keys($blog_arr);
-        $data           = $this->_model->table($table_arr[$type])
-        ->where(array('blog_id' => array('IN', $blog_id_arr), 'add_time' => array('IN', $add_time_arr)))
-        ->field('blog_id,add_time,hits,comments,diggs')// - 1 AS `add_time`
-        ->key_column('blog_id')
-        ->select();
-        $un_match       = $type;
-
-        foreach($data as $k => $v) {
-
-            if ($blog_arr[$k] != $v['add_time']) {//id与时间不匹配
-                $un_match .= ",{$k}({$blog_arr[$k]}) => {$k}({$v['add_time']})";
-                unset($data[$k]);
-            }
-            else {
-                unset($data[$k]['blog_id'], $data[$k]['add_time']);
-            }
-        }
-
-        $un_match != $type && $this->triggerError(__METHOD__ . ': ' . __LINE__ . ',' . $un_match, E_USER_WARNING);
-
-        return $data;
-    }//end _getMetaInfo
-
-    /**
      * 统计点击量
      *
      * @author          mrmsl <msl-138@163.com>
@@ -170,17 +106,11 @@ class AjaxController extends CommonController {
             $this->_ajaxReturn(false);
         }
         $blog           = 0 === strpos($blog, ',') ? substr($blog, 1) : $blog;
-        $blog_arr       = $blog ? explode(',', $blog) : array();
         $miniblog       = 0 === strpos($miniblog, ',') ? substr($miniblog, 1) : $miniblog;
-        $miniblog_arr   = $miniblog ? explode(',', $miniblog) : array();
-
-        if (($len_1 = count($blog_arr)) > PAGE_SIZE || ($len_2 = count($miniblog_arr)) >PAGE_SIZE) {//长度限制判断
-            $this->triggerError(__METHOD__ . ': ' . __LINE__ . ',' . $len_1 . (isset($len_2) ? ',' . $len_2 : '') . ' > ' . PAGE_SIZE);
-            $this->_ajaxReturn(false);
-        }
-
-        $miniblog_data  = $this->_getMetaInfo('miniblog', $miniblog_arr);
-        $blog_data      = $this->_getMetaInfo('blog', $blog_arr);
+        $field_arr      = 'blog_id,add_time';
+        $field          = 'blog_id,add_time,hits,comments,diggs';
+        $miniblog_data  = $this->_getPairsData($field_arr, $miniblog, $field, 'blog_id', TB_MINIBLOG);
+        $blog_data      = $this->_getPairsData($field_arr, $blog, $field, 'blog_id', TB_BLOG);
 
         $this->_ajaxReturn(array('blog' => $blog_data, 'miniblog' => $miniblog_data, 'success' => true));
     }//end metaInfoAction
